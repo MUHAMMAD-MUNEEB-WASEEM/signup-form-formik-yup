284fe61047d5d2b9c896fd5d69553bf76be6aabb
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-01-20 18:41:00 +0500

    complete signup form

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import './App.css';
+import UserFormWithFormikValidationContext from './components/signupForm';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: "black",
+    alignItems: "center",
+    textAlign: "center",
+    fontWeight: "bolder",
+    fontSize: 30,
+  },
+  back: {
+    backgroundColor: "orange",
+  }
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+        <AppBar position="static" className={classes.back}>
+             <Toolbar>
+                        
+                 <Typography variant="h6" className={classes.title}>
+                   SIGNUP FORM
+                  </Typography>
+                        
+                  </Toolbar>
+        </AppBar>
+      <UserFormWithFormikValidationContext />
+    </div>
+  );
+}
+
+export default App;
